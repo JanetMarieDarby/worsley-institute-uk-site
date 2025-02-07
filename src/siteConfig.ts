4442c4487b16954e9c0c8ed7a25250b7eee18960
DrDrij <dr.drij@gmail.com>
2025-02-07 15:39:49 +1100

    Convert whole site to just a HTML file.

--- src/siteConfig.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    siteName: "Worsley Institute - UK training school",
-    siteDescription: "Worsley Institute - UK training school",
-    siteUrl: "https://gridsome-starter-bleda.netlify.com",
-    // icon: 'src/favicon.png',
-};
